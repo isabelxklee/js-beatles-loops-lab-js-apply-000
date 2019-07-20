a37aa18c8907c09423793c3c53b1ed794beedbd3
capable-daemon-6664 <hello@isabelklee.co>
2019-07-20 15:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ for (let i = 0; i < musicians.length; i++) {
 
 // This is a while loop.
 function johnLennonFacts () {
-  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
   let johnLennonFacts = [];
 
 let i = 0;
