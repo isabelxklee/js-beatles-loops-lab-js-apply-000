6fbcd5e1fbf46c86c800965cd421c87e639d3c46
capable-daemon-6664 <hello@isabelklee.co>
2019-07-20 15:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+function theBeatlesPlay () {
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  let statement = [];
+
+for (let i = 0; i < 1; i++) {
+   console.log (musicians[i] + ' plays ' + instruments[i]);
+      statement.push(musicians[i] + ' plays ' + instruments[i]);
+   }
+   return statement;
+}
+
+
+
+// This is a new function.
+function johnLennonFacts () {
+  const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+}
+
+let i = 0;
+while (i < facts.length) {
+  johnLennonFacts.push(facts[i] += "!!!");
+  i++;
+  console.log(johnLennonFacts);
+}
+
+
+
+
+
+
+
+
