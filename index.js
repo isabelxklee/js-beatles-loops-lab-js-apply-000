33c6a27ca866639120f9405f22930a55965fd9e1
capable-daemon-6664 <hello@isabelklee.co>
2019-07-20 15:57:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// This is a for loop.
+
 function theBeatlesPlay () {
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
@@ -9,9 +11,7 @@ for (let i = 0; i < musicians.length; i++) {
    return statement;
 }
 
-
-
-// This is a new function.
+// This is a while loop.
 function johnLennonFacts () {
   const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
 }
